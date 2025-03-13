7ea3744b4332119fa5df97e4bf4018e12384bcc8
Kristin Salvacion <nitnitks@gmail.com>
2025-03-13 12:20:08 -0400

    added expense model and expenses page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/expenses.dart';
+
 void main() {
-  runApp(const MaterialApp(
-    
-  ));
+  runApp(
+    const MaterialApp(
+      home: Expenses()
+    ),
+  );
 }
