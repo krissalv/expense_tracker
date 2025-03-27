18a0b173243d58ee7d23caf0ba146e1c5b348165
Kristin Salvacion <nitnitks@gmail.com>
2025-03-27 12:01:02 -0400

    added data to card

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -7,13 +7,14 @@ enum Category {food, travel, leisure, work}
 class Expense{
   Expense({
     required this.title,
-    required this.amount, 
-    required this.date
-    }) : id = uuid.v4();
-
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+  
   final String id;
   final String title;
   final double amount;
   final DateTime date;
-  final Category category;
+  final Category category; // leisure Expense(category : 'leisure') 
 }
